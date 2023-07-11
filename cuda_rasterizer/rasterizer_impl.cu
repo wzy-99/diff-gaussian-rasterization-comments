16e187f814faa87aac63d452f3f3b8b5a64e9aaa
WZY99 <lhwzy99@163.com>
2023-07-11 20:02:33 +0800

    Update rasterizer_impl.cu

--- cuda_rasterizer/rasterizer_impl.cu
+++ cuda_rasterizer/rasterizer_impl.cu
@@ -217,6 +217,46 @@ int CudaRasterizer::Rasterizer::forward(
 	const bool prefiltered,
 	float* out_color,
 	int* radii)
+/**
+输入参数
+
+geometryBuffer：返回几何缓冲区的函数
+binningBuffer：返回分箱缓冲区的函数
+imageBuffer：返回图片缓冲区的函数
+P：3D点的数量
+D：3D点的维度
+M：球谐函数的次数
+background：背景图像数据
+width：图像的宽度
+height：图像的高度
+means3D：3D点的坐标数组
+shs：球谐函数数组
+colors_precomp：预计算的颜色数组
+opacities：不透明度数组
+scales：缩放因子数组
+scale_modifier：缩放因子的修改值
+rotations：旋转角度数组
+cov3D_precomp：预计算的3D协方差数组
+viewmatrix：视图变换矩阵
+projmatrix：投影矩阵
+cam_pos：相机位置数组
+tan_fovx：x方向的焦距
+tan_fovy：y方向的焦距
+prefiltered：是否应用预过滤
+out_color：渲染后的颜色数组
+radii：点的半径数组（可选参数，默认为nullptr）
+
+该函数执行以下操作：
+
+1. 计算焦距 `focal_y` 和 `focal_x`。
+2. 分配并初始化几何状态对象 `geomState` 和图像状态对象 `imgState`。
+3. 对每个高斯进行预处理（转换、边界、SH转RGB等）。
+4. 计算每个 tile 的高斯数量，并进行前缀和运算。
+5. 根据排序键对高斯索引进行排序。
+6. 标识排序后高斯索引在每个 tile 中的范围。
+7. 并行处理每个 tile 的高斯融合。
+8. 返回渲染的高斯数量。
+**/
 {
 	const float focal_y = height / (2.0f * tan_fovy);
 	const float focal_x = width / (2.0f * tan_fovx);
@@ -230,6 +270,7 @@ int CudaRasterizer::Rasterizer::forward(
 		radii = geomState.internal_radii;
 	}
 
+	// 这段代码用于确定线程块和线程网格的维度
 	dim3 tile_grid((width + BLOCK_X - 1) / BLOCK_X, (height + BLOCK_Y - 1) / BLOCK_Y, 1);
 	dim3 block(BLOCK_X, BLOCK_Y, 1);
 
@@ -275,14 +316,45 @@ int CudaRasterizer::Rasterizer::forward(
 	// E.g., [2, 3, 0, 2, 1] -> [2, 5, 5, 7, 8]
 	cub::DeviceScan::InclusiveSum(geomState.scanning_space, geomState.scan_size,
 		geomState.tiles_touched, geomState.point_offsets, P);
+	/**
+	这段代码使用CUB库的`DeviceScan`函数来计算被高斯点触碰到的所有tile计数的前缀和。
+	
+	具体地说，它接受以下参数：
+	
+	- `geomState.scanning_space`：一个用于临时存储扫描结果的缓冲区。
+	- `geomState.scan_size`：缓冲区的大小（以字节为单位）。
+	- `geomState.tiles_touched`：一个存储每个高斯点触碰到的tile数量的数组。
+	- `geomState.point_offsets`：用于存储计算后的前缀和结果的数组。
+	- `P`：高斯点的数量。
+	
+	函数通过对`geomState.tiles_touched`数组进行前缀和操作，计算每个高斯点之前触碰到的tile的总数。例如，如果传入的`tiles_touched`数组为`[2, 3, 0, 2, 1]`，则计算后的结果将为`[2, 5, 5, 7, 8]`。这个前缀和数组存储在`geomState.point_offsets`中，可以在后续步骤中使用。
+ 	**/
 
 	// Retrieve total number of Gaussian instances to launch and resize aux buffers
 	int num_rendered;
 	cudaMemcpy(&num_rendered, geomState.point_offsets + P - 1, sizeof(int), cudaMemcpyDeviceToHost);
+	/**
+	这段代码用于获取要渲染的高斯点的总数量，并调整辅助缓冲区的大小。
+	
+	具体而言，代码通过使用`cudaMemcpy`函数从设备内存复制数据到主机内存，将`geomState.point_offsets`数组中的值复制到`num_rendered`变量中。`geomState.point_offsets`是一个存储了经过前缀和计算后的每个高斯点所触碰到的tile的累计数量的数组。通过将`P - 1`作为偏移量，我们可以获取到最后一个tile的累计数量，从而得到要渲染的高斯点的总数量。
+	
+	获取到总数量后，可以使用这个值来调整辅助缓冲区的大小，以确保它足够容纳所有要渲染的高斯点。这样可以避免缓冲区溢出或浪费存储空间。
+ 	**/
 
 	int binning_chunk_size = required<BinningState>(num_rendered);
 	char* binning_chunkptr = binningBuffer(binning_chunk_size);
 	BinningState binningState = BinningState::fromChunk(binning_chunkptr, num_rendered);
+	/**
+	这段代码用于根据要渲染的高斯点的总数量，分配并初始化`BinningState`对象所需的内存空间。
+	
+	首先，代码调用`required`函数，传入`num_rendered`作为参数，以确定分配内存所需的大小。这个函数返回一个值，表示为了满足所需的内存大小，需要的字节数。这个值存储在`binning_chunk_size`变量中。
+	
+	接下来，代码使用`binningBuffer`函数，传入`binning_chunk_size`作为参数，从`binningBuffer`对象中分配一块大小为`binning_chunk_size`的内存空间，并将其返回给`binning_chunkptr`指针。
+	
+	然后，代码使用`BinningState::fromChunk`静态函数，传入`binning_chunkptr`和`num_rendered`作为参数，将之前分配的内存空间初始化为`BinningState`对象。这个函数会创建一个`BinningState`对象，将内存块的首地址和要渲染的高斯点数量传入，并返回初始化后的对象。
+	
+	最终，通过这段代码，我们分配了足够的内存来容纳`BinningState`对象，并将其初始化为具有指定大小的内存块。这将为之后的处理步骤提供必要的空间和状态。
+ 	**/
 
 	// For each instance to be rendered, produce adequate [ tile | depth ] key 
 	// and corresponding dublicated Gaussian indices to be sorted
@@ -296,6 +368,25 @@ int CudaRasterizer::Rasterizer::forward(
 		radii,
 		tile_grid
 		);
+	/**
+	这段代码用于为每个要渲染的高斯点生成适当的`[ tile | depth ]`键和相应的重复高斯点索引，以便进行排序。
+	
+	代码调用了名为`duplicateWithKeys`的CUDA内核函数，使用了一组参数来执行操作。具体而言，使用了以下参数：
+	
+	- `(P + 255) / 256`：用于计算启动线程块的数量。这个表达式保证了足够数量的线程块来处理高斯点的数量（`P`）。
+	- `P`：高斯点的数量。
+	- `geomState.means2D`：二维均值点的数组。
+	- `geomState.depths`：深度值的数组。
+	- `geomState.point_offsets`：每个高斯点所触碰到的tile的累计数量的数组。
+	- `binningState.point_list_keys_unsorted`：未排序的高斯点键的数组。
+	- `binningState.point_list_unsorted`：未排序的高斯点索引的数组。
+	- `radii`：高斯点的半径的数组。
+	- `tile_grid`：存储tile的数据结构。
+	
+	这段代码的目标是为每个高斯点生成适当的`[ tile | depth ]`键（用于排序），并在排序过程中将高斯点的索引进行重复。通过这个操作，每个高斯点将在列表中出现多次，每次使用不同的`[ tile | depth ]`键。这样可以确保高斯点在排序后以适当的位置进行渲染。
+	
+	调用CUDA内核函数时，启动足够数量的线程块，并使用不同的线程处理不同的高斯点。这个内核函数执行每个高斯点的处理，并根据给定的参数生成相应的键和索引。所有线程块的处理将保证所有高斯点的键和索引都得到正确生成，并准备进行排序操作。
+ 	**/
 
 	int bit = getHigherMsb(tile_grid.x * tile_grid.y);
 
